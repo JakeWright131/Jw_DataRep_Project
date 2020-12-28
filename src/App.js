f15affb171076769e0d69b126b936a4b317da08c
unknown <jakewright131@gmail.com>
2020-12-28 19:07:38 +0000

    added edit function

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import { Navbar, Nav } from 'react-bootstrap';
 import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
 import { Create } from './components/create';
 import { Read } from './components/read';
+import { Edit } from './components/edit';
 
 class App extends Component {
   render() {
@@ -31,7 +32,7 @@ class App extends Component {
   <Route path='/' component={Content} exact/>
   <Route path='/add' component={Create} exact/>
   <Route path='/view' component={Read} exact/>
-
+  <Route path='/edit/:id' component={Edit} exact/>
 </Switch>
 
         
